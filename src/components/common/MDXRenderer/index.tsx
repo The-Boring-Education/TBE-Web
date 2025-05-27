d57e4a025f9bf57689561312ad84207cf63631a2
Sourav <sourav.khilari.it26@heritageit.edu.in>
2025-05-27 10:34:28 +0530

    `Update MDXRenderer component: changed whitespace property in pre element`

--- src/components/common/MDXRenderer/index.tsx
+++ src/components/common/MDXRenderer/index.tsx
@@ -66,7 +66,7 @@ const MDXRenderer = ({ mdxSource, actions }: MDXRendererProps) => {
 
     return (
       `<div class="relative mb-4">` +
-      `<pre class="bg-accent overflow-x-auto hover:bg-greyLight transition border px-4 py-6 rounded">` +
+      `<pre class="bg-accent overflow-x-auto whitespace-pre hover:bg-greyLight transition border px-4 py-6 rounded">` +
       `<code class="language-${lang}">${md.utils.escapeHtml(code)}</code>` +
       `</pre>` +
       `</div>`
