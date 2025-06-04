f70e3f0ecb54b4231ac7914ee3a8c7ea5057460d
Sachin <sachinkshuklaoo7@gmail.com>
2025-06-04 12:33:55 +0530

    fix pdf

--- src/hooks/usePDFFile.ts
+++ src/hooks/usePDFFile.ts
@@ -1,20 +1,22 @@
 import { extractSkillsFromText } from '@/utils';
 import { useState } from 'react';
-import pdfToText from 'react-pdftotext';
+// import pdfToText from 'react-pdftotext';
 
+// FIXME: Uncomment the pdfToText import when the library is installed and configured correctly
 const usePDFFile = () => {
   const [extractedSkills, setExtractedSkills] = useState<string[]>([]);
   const [file, setFile] = useState<File | null>(null);
 
   const extractTextFromPDF = async (file: File) => {
-    pdfToText(file)
-      .then((text) => {
-        const extractedSkills = extractSkillsFromText(text);
-        setExtractedSkills(extractedSkills);
-      })
-      .catch((error) =>
-        console.error('Failed to extract text from pdf', error)
-      );
+    // pdfToText(file)
+    //   .then((text) => {
+    //     const extractedSkills = extractSkillsFromText(text);
+    //     setExtractedSkills(extractedSkills);
+    //   })
+    //   .catch((error) =>
+    //     console.error('Failed to extract text from pdf', error)
+    //   );
+    return [];
   };
 
   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
