8e88fd3eff1969326bc9d2740b989fa94457d7d0
Sachin <sachinkshuklaoo7@gmail.com>
2025-06-04 12:01:31 +0530

    Refactor ESLint configuration by removing unused rules and plugins, and updating the extends section for improved code quality and consistency

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,73 +3,29 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
-    jest: true,
   },
-  plugins: [
-    '@typescript-eslint',
-    'simple-import-sort',
-    'unused-imports',
-    'react',
-    'react-hooks',
-  ],
+  plugins: ['@typescript-eslint', 'simple-import-sort', 'unused-imports'],
   extends: [
     'eslint:recommended',
     'next',
     'next/core-web-vitals',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
+    'prettier',
   ],
   rules: {
-    // General rules
     'no-unused-vars': 'warn',
-    'no-console': ['warn', { allow: ['warn'] }],
-    'no-debugger': 'warn',
-    'no-duplicate-imports': 'warn',
-    'no-var': 'warn',
-    'prefer-const': 'warn',
-    'no-multiple-empty-lines': ['warn', { max: 1, maxEOF: 0 }],
-    'no-trailing-spaces': 'warn',
-    'eol-last': 'warn',
-
-    // TypeScript specific rules
+    'no-console': 'warn',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'warn',
-    '@typescript-eslint/consistent-type-imports': [
-      'warn',
-      { prefer: 'type-imports' },
-    ],
-
-    // React specific rules
     'react/no-unescaped-entities': 'off',
+
     'react/display-name': 'off',
     'react/jsx-curly-brace-presence': [
       'warn',
       { props: 'never', children: 'never' },
     ],
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react-hooks/rules-of-hooks': 'warn',
-    'react-hooks/exhaustive-deps': 'warn',
-    'react/jsx-sort-props': [
-      'warn',
-      {
-        callbacksLast: true,
-        shorthandFirst: true,
-        ignoreCase: true,
-        reservedFirst: true,
-      },
-    ],
 
-    // Accessibility rules
-    'jsx-a11y/anchor-is-valid': 'warn',
-    'jsx-a11y/click-events-have-key-events': 'warn',
-    'jsx-a11y/no-static-element-interactions': 'warn',
-
-    // Import rules
+    //#region  //*=========== Unused Import ===========
+    '@typescript-eslint/no-unused-vars': 'off',
     'unused-imports/no-unused-imports': 'warn',
     'unused-imports/no-unused-vars': [
       'warn',
@@ -80,55 +36,50 @@ module.exports = {
         argsIgnorePattern: '^_',
       },
     ],
-    'simple-import-sort/exports': 'warn',
-    'simple-import-sort/imports': [
-      'warn',
-      {
-        groups: [
-          // ext library & side effect imports
-          ['^@?\\w', '^\\u0000'],
-          // {s}css files
-          ['^.+\\.s?css$'],
-          // Lib and hooks
-          ['^@/hooks'],
-          // static data
-          ['^@/data'],
-          // components
-          ['^@/components', '^@/container'],
-          // Other imports
-          ['^@/'],
-          // relative paths up until 3 level
-          [
-            '^\\./?$',
-            '^\\.(?!/?$)',
-            '^\\.\\./?$',
-            '^\\.\\.(?!/?$)',
-            '^\\.\\./\\.\\./?$',
-            '^\\.\\./\\.\\.(?!/?$)',
-            '^\\.\\./\\.\\./\\.\\./?$',
-            '^\\.\\./\\.\\./\\.\\.(?!/?$)',
-          ],
-          ['^@/types'],
-          // other that didnt fit in
-          ['^'],
-        ],
-      },
-    ],
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
+    '@typescript-eslint/no-explicit-any': 'off',
+    //#endregion  //*======== Unused Import ===========
+
+    // //#region  //*=========== Import Sort ===========
+    // 'simple-import-sort/exports': 'warn',
+    // 'simple-import-sort/imports': [
+    //   'warn',
+    //   {
+    //     groups: [
+    //       // ext library & side effect imports
+    //       ['^@?\\w', '^\\u0000'],
+    //       // {s}css files
+    //       ['^.+\\.s?css$'],
+    //       // Lib and hooks
+    //       ['^@/lib', '^@/hooks'],
+    //       // static data
+    //       ['^@/data'],
+    //       // components
+    //       ['^@/components', '^@/container'],
+    //       // zustand store
+    //       ['^@/store'],
+    //       // Other imports
+    //       ['^@/'],
+    //       // relative paths up until 3 level
+    //       [
+    //         '^\\./?$',
+    //         '^\\.(?!/?$)',
+    //         '^\\.\\./?$',
+    //         '^\\.\\.(?!/?$)',
+    //         '^\\.\\./\\.\\./?$',
+    //         '^\\.\\./\\.\\.(?!/?$)',
+    //         '^\\.\\./\\.\\./\\.\\./?$',
+    //         '^\\.\\./\\.\\./\\.\\.(?!/?$)',
+    //       ],
+    //       ['^@/types'],
+    //       // other that didnt fit in
+    //       ['^'],
+    //     ],
+    //   },
+    // ],
+    // //#endregion  //*======== Import Sort ===========
   },
   globals: {
     React: true,
     JSX: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
   },
 };
