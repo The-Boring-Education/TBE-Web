b1c803d9bcb8483a2001b32c64ccd9ddc0fdae8e
Sachin <sachinkshuklaoo7@gmail.com>
2025-06-29 12:15:36 +0530

    Refactor: Simplify OpenSourceStatsSection component structure
    - Converted the OpenSourceStatsSection function definition to a concise arrow function for improved readability.
    - Removed unnecessary return statement to streamline the component's code.

--- src/components/containers/Page/OpenSource/OpenSourceStatsSection.tsx
+++ src/components/containers/Page/OpenSource/OpenSourceStatsSection.tsx
@@ -10,8 +10,7 @@ import {
 } from '@/components';
 import { OPEN_SOURCE_BENEFITS, OPEN_SOURCE_STATS } from '@/constant';
 
-const OpenSourceStatsSection = () => {
-  return (
+const OpenSourceStatsSection = () => (
     <GradientContainer className='border-borderColor2 p-8'>
       {/* Header */}
       <FlexContainer direction='col' itemCenter className='mb-12'>
@@ -169,6 +168,5 @@ const OpenSourceStatsSection = () => {
       </FlexContainer>
     </GradientContainer>
   );
-};
 
 export default OpenSourceStatsSection;
