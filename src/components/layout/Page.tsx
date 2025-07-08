c632e52d2845fa865e71bbd27a38d5c053bac9a5
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-07-08 14:51:34 +0530

    removed demo gamification from landing

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 import { Footer, Navbar } from '@/components';
-import GamificationDemo from '@/components/common/GamificationDemo';
 import { envConfig } from '@/constant';
 import type { PageLayoutProps } from '@/interfaces';
 
@@ -40,9 +39,6 @@ const PageLayout = ({ children }: PageLayoutProps) => {
         {children}
       </motion.div>
       <Footer />
-      
-      {/* Development/Testing Demo - Remove in production */}
-      {process.env.NODE_ENV === 'development' && <GamificationDemo />}
     </main>
   );
 };
