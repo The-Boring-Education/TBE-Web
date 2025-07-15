e2ed5e1fb6e20766ad8c5dceb5889faf7c3fbbfb
Nitin <153427975+NitinGurawaliya@users.noreply.github.com>
2025-07-15 15:05:38 +0530

    comments remove

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -33,7 +33,7 @@ const AppContent = ({
 
     if (!isOnboarded && isAuth && router.pathname !== routes.onboarding) {
       // Redirect to external onboarding app
-      const onboardingBaseUrl = envConfig.ONBOARDING_APP_URL; // TODO: Replace with actual onboarding app URL
+      const onboardingBaseUrl = envConfig.ONBOARDING_APP_URL;
       const params = new URLSearchParams({
         userId: user?.id || '',
         from: 'webapp',
