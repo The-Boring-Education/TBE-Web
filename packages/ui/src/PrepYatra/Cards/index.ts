af496b1905214d97f94e1c97393881a16bcf8ebf
NitinGurawaliya <221030201@juitsolan.in>
2025-10-08 22:45:13 +0530

    remove ui package

--- packages/ui/src/PrepYatra/Cards/index.ts
+++
@@ -1,3 +0,0 @@
-export {default as ChallengeCard} from "./ChallengeCard";
-export {default as ChallengeShareCard} from "./ChallengeShareCard";
-export {default as FeatureCards} from "./FeatureCards";
