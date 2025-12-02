56cc6d43303bba2a9272162c019e57c7cc2fdbf7
Nikhil273 <nikhilmaurya.it@gmail.com>
2025-12-02 15:09:17 +0530

    Refactor GradientContainer styles and improve hover effects

--- packages/components/src/containers/Page/common/GradientContainer.tsx
+++ packages/components/src/containers/Page/common/GradientContainer.tsx
@@ -1,16 +1,15 @@
-import type { GradientContainerProps } from '@tbe/interface';
+import type { GradientContainerProps } from "@tbe/interface";
 
 // To Put Border Color => border-borderColor${variant}
 const GradientContainer = ({
   children,
-  className = '',
+  className = "",
   backgroundColor,
-  childrenClassName = 'p-3',
+  childrenClassName = "p-3",
 }: GradientContainerProps) => (
   <div
-    className={`flex-auto rounded-2 border ${className} ${
-      backgroundColor ?? 'bg-white'
-    }`}
+    className={`flex-auto rounded-2 border ${className} ${backgroundColor ?? "bg-white"}hover:shadow-2xl
+      transition-all duration-300 hover:scale-105`}
   >
     <div className={`rounded-2 ${childrenClassName}`}>{children}</div>
   </div>
