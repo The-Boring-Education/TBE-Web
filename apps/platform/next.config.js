352c6d28db4e6e2dcc66dfdb39d949f573e18cc1
NitinGurawaliya <221030201@juitsolan.in>
2026-02-03 00:01:25 +0530

    fix next cinfig file

--- apps/platform/next.config.js
+++ apps/platform/next.config.js
@@ -161,11 +161,9 @@ if (process.env.NODE_ENV === 'production') {
     automaticVercelMonitors: true,
   };
 
-  module.exports = withSentryConfig(
-    withTM(nextConfig),
-    sentryWebpackPluginOptions
-  );
+  module.exports = withSentryConfig(withTM(nextConfig), sentryWebpackPluginOptions);
 } else {
   // Skip Sentry in development to avoid OpenTelemetry errors
   module.exports = withTM(nextConfig);
 }
+
