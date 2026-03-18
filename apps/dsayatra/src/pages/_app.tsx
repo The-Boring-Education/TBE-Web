9c8b7a3875bccd48e71a8ad32556ea432c56248e
Shivani Jha <shivanijhavats@gmail.com>
2026-03-19 03:42:16 +0530

    Add new DSA Yatra favicon and update _app.tsx

--- apps/dsayatra/src/pages/_app.tsx
+++ apps/dsayatra/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@tbe/components/styles/common.css";
 import "@/index.css";
 
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import Layout from "@/components/Layout";
 import { OnboardingCheck } from "@/components/OnboardingCheck";
@@ -9,11 +10,16 @@ import { Providers } from "@/components/Providers";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Providers>
-      <OnboardingCheck />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Providers>
+    <>
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/dsayatra_favicon.svg" />
+      </Head>
+      <Providers session={pageProps.session}>
+        <OnboardingCheck />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Providers>
+    </>
   );
 }
