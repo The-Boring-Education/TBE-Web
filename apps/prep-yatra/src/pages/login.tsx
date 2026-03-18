b08fe4325f989e597ff61a8b2614277af9f09c76
Sachin <sachinkshuklaoo7@gmail.com>
2026-03-18 11:55:17 +0530

    Refactor login page to dynamically import LoginCardNew component
    - Replaced static import of LoginCardNew with dynamic import to improve performance and prevent server-side rendering issues.
    - This change enhances the loading behavior of the login page in the prep-yatra application.

--- apps/prep-yatra/src/pages/login.tsx
+++ apps/prep-yatra/src/pages/login.tsx
@@ -1,8 +1,16 @@
 import { useAuth } from "@tbe/auth";
-import { Footer, LoginCardNew, Navbar } from "@tbe/components";
+import { Footer, Navbar } from "@tbe/components";
 import { InstallButton } from "@tbe/components";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+
+const LoginCardNew = dynamic(
+  () => import("@tbe/components").then((mod) => mod.LoginCardNew),
+  {
+    ssr: false,
+  },
+);
 
 const Auth = () => {
   const router = useRouter();
